870716c70700cc42f2aa0173b97401cc7f6e7ffe
JulianJT <j.d.tjintjelaar@gmail.com>
2020-10-20 23:02:55 +0200

    Code formatting

--- search.js
+++ search.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $("#anythingSearch").on("keyup", function () {
     var value = $(this).val().toLowerCase();
     $("#myDIV div").filter(function () {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
     });
   });
 });
