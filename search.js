c65dcce3b506586cba9ceb64c84e5a87ad315613
JulianJT <j.d.tjintjelaar@gmail.com>
2020-10-20 20:30:10 +0200

    added search

---
+++ search.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $("#anythingSearch").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#myDIV div").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
